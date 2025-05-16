186ebe649096e9359648546a62aff5118ddf416c
Ieva Busenberga <Ieva.Busenberga@edu.rtu.lv>
2025-05-16 10:01:41 +0300

    Add files via upload

---
+++ euronics.py
@@ -0,0 +1,46 @@
+import requests
+import bs4
+import xlsxwriter
+import re
+
+
+main_url = 'https://www.euronics.lv/'
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36'}
+
+data = [['Nosaukums', 'Сena EUR']]
+
+def get_soup(url):
+    res = requests.get(url, headers=headers)
+    return bs4.BeautifulSoup(res.text, 'html.parser')
+
+iphone_page = get_soup(main_url + 'telefoni/viedtalruni/visi-viedtalruni/apple?f=CgVwcmljZSIFCEUQjx0wAw&p=1')
+iphones = iphone_page.find_all('article', class_='product-card')
+
+
+for item in iphones:
+    # Название товара
+    title_tag = item.find('span', class_='product-card__title')
+    title = title_tag.get_text(strip=True) if title_tag else 'Nosaukums nav atrasts'
+
+    # Цена
+    price_tag = item.find('div', class_='price')
+    if price_tag:
+        parts = list(price_tag.stripped_strings)
+        raw_price = ''.join(parts)
+        prices_found = re.findall(r'\d+[.,]?\d*', raw_price)
+
+        if prices_found:
+            price = prices_found[-1].replace(',', '.').strip()
+        else:
+            price = 'nav'
+    else:
+        price = 'nav'
+
+    data.append([title, price])
+
+# Запись в Excel
+with xlsxwriter.Workbook('Euronics.xlsx') as workbook:
+    worksheet = workbook.add_worksheet()
+    for row_num, row_data in enumerate(data):
+        worksheet.write_row(row_num, 0, row_data)
---
+++ rdveikals.py
@@ -0,0 +1,77 @@
+import requests
+import bs4
+import xlsxwriter
+
+
+main_url = 'https://www.rdveikals.lv/'
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36'}
+
+data = [['Nosaukums', 'Cena EUR']]
+
+def get_soup(url):
+    res = requests.get(url, headers=headers)
+    return bs4.BeautifulSoup(res.text, 'html.parser')
+
+iphone_page = get_soup(main_url+'categories/lv/388/sort/5/filter/0_0_74_0/page/1/Mobilie-telefoni.html')
+iphones = iphone_page.findAll('div', class_='product__info')
+for item in iphones:
+    title = item.find('h3', class_='product__title').a.get_text(separator=' ', strip=True)
+    price = item.find('p', class_='price').find(text=True).strip()
+    data.append([title, price])
+
+iphone_page = get_soup(main_url+'categories/lv/388/sort/5/filter/0_0_74_0/page/2/Mobilie-telefoni.html')
+iphones = iphone_page.findAll('div', class_='product__info')
+for item in iphones:
+    title = item.find('h3', class_='product__title').a.get_text(separator=' ', strip=True)
+    price = item.find('p', class_='price').find(text=True).strip()
+    data.append([title, price])
+
+iphone_page = get_soup(main_url+'categories/lv/388/sort/5/filter/0_0_74_0/page/3/Mobilie-telefoni.html')
+iphones = iphone_page.findAll('div', class_='product__info')
+for item in iphones:
+    title = item.find('h3', class_='product__title').a.get_text(separator=' ', strip=True)
+    price = item.find('p', class_='price').find(text=True).strip()
+    data.append([title, price])
+
+iphone_page = get_soup(main_url+'categories/lv/388/sort/5/filter/0_0_74_0/page/4/Mobilie-telefoni.html')
+iphones = iphone_page.findAll('div', class_='product__info')
+for item in iphones:
+    title = item.find('h3', class_='product__title').a.get_text(separator=' ', strip=True)
+    price = item.find('p', class_='price').find(text=True).strip()
+    data.append([title, price])
+
+iphone_page = get_soup(main_url+'categories/lv/388/sort/5/filter/0_0_74_0/page/5/Mobilie-telefoni.html')
+iphones = iphone_page.findAll('div', class_='product__info')
+for item in iphones:
+    title = item.find('h3', class_='product__title').a.get_text(separator=' ', strip=True)
+    price = item.find('p', class_='price').find(text=True).strip()
+    data.append([title, price])
+
+iphone_page = get_soup(main_url+'categories/lv/388/sort/5/filter/0_0_74_0/page/6/Mobilie-telefoni.html')
+iphones = iphone_page.findAll('div', class_='product__info')
+for item in iphones:
+    title = item.find('h3', class_='product__title').a.get_text(separator=' ', strip=True)
+    price = item.find('p', class_='price').find(text=True).strip()
+    data.append([title, price])
+
+iphone_page = get_soup(main_url+'categories/lv/388/sort/5/filter/0_0_74_0/page/7/Mobilie-telefoni.html')
+iphones = iphone_page.findAll('div', class_='product__info')
+for item in iphones:
+    title = item.find('h3', class_='product__title').a.get_text(separator=' ', strip=True)
+    price = item.find('p', class_='price').find(text=True).strip()
+    data.append([title, price])
+
+iphone_page = get_soup(main_url+'categories/lv/388/sort/5/filter/0_0_74_0/page/8/Mobilie-telefoni.html')
+iphones = iphone_page.findAll('div', class_='product__info')
+for item in iphones:
+    title = item.find('h3', class_='product__title').a.get_text(separator=' ', strip=True)
+    price = item.find('p', class_='price').find(text=True).strip()
+    data.append([title, price])
+
+with xlsxwriter.Workbook('rdveikalss.xlsx') as workbook:
+    worksheet = workbook.add_worksheet()
+
+    for row_num, info in enumerate(data):
+        worksheet.write_row(row_num, 0, info)
+
